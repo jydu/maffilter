30d78b61bea5a19f375d09bfcf503903b2d27309
Julien Dutheil <jy.dutheil@gmail.com>
2013-02-27 21:35:47 +0100

    Added help message.

--- MafFilter/MafFilter.cpp
+++ MafFilter/MafFilter.cpp
@@ -79,7 +79,12 @@ using namespace bpp;
 
 void help()
 {
-
+  (*ApplicationTools::message << "__________________________________________________________________________").endLine();
+  (*ApplicationTools::message << "maffilter name1=value1 name2=value2").endLine();
+  (*ApplicationTools::message << "      ... param=option_file").endLine();
+  (*ApplicationTools::message).endLine();
+  (*ApplicationTools::message << "  Refer to the MafFilter Manual for a list of available options.").endLine();
+  (*ApplicationTools::message << "__________________________________________________________________________").endLine();
 }
 
 int main(int args, char** argv)
