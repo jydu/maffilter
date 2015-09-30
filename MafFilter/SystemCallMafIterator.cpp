68afe2c59f374b63968dae07e19ac2de77e94190
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2015-09-30 09:57:29 +0200

    Added missing files for SystemCallMafIterator.

---
+++ MafFilter/SystemCallMafIterator.cpp
@@ -0,0 +1,75 @@
+//
+// File: SystemCallMafIterator.cpp
+// Authors: Julien Dutheil
+// Created: Tue Sep 29 2015
+//
+
+/*
+Copyright or © or Copr. Julien Y. Dutheil, (2015)
+
+This software is a computer program whose purpose is to provide classes
+for sequences analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "SystemCallMafIterator.h"
+
+using namespace bpp;
+using namespace std;
+
+MafBlock* SystemCallMafIterator::analyseCurrentBlock_() throw (Exception) {
+  currentBlock_ = iterator_->nextBlock();
+  auto_ptr<AlignedSequenceContainer> aln(currentBlock_->getAlignment().clone());
+  
+  //We translate sequence names to avoid compatibility issues
+  vector<string> names(aln->getNumberOfSequences());
+  for (size_t i = 0; i < names.size(); ++i) {
+    names[i] = "seq" + TextTools::toString(i);
+  }
+  aln->setSequencesNames(names);
+  
+  //Write sequences to file:
+  alnWriter_->writeAlignment(inputFile_, *aln, true);
+
+  //Call the external program:
+  system(call_.c_str());
+
+  //Then read and assign the realigned sequences:
+  auto_ptr<SiteContainer> result(alnReader_->readAlignment(inputFile_, &AlphabetTools::DNA_ALPHABET));
+  for (size_t i = 0; i < currentBlock_->getNumberOfSequences(); ++i) {
+    auto_ptr<MafSequence> mseq(currentBlock_->getSequence(i).clone());
+    //NB: we discard any putative score associated to this sequence.
+    string name = "seq" + TextTools::toString(i);
+    mseq->setContent(dynamic_cast<const BasicSequence&>(result->getSequence(name)).toString()); //NB shall we use getContent here?
+    currentBlock_->getAlignment().setSequence(i, *mseq);
+  }
+
+  //Done:
+  return currentBlock_;
+}
+
---
+++ MafFilter/SystemCallMafIterator.h
@@ -0,0 +1,110 @@
+//
+// File: SystemCallMafIterator.h
+// Authors: Julien Dutheil
+// Created: Tue Sep 29 2015
+//
+
+/*
+Copyright or © or Copr. Julien Y. Dutheil, (2015)
+
+This software is a computer program whose purpose is to provide classes
+for sequences analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _SYSTEMCALLMAFITERATOR_H_
+#define _SYSTEMCALLMAFITERATOR_H_
+
+#include <Bpp/Seq/Io/Maf/MafIterator.h>
+#include <Bpp/Seq/Io/ISequence.h>
+#include <Bpp/Seq/Io/OSequence.h>
+
+//From the STL:
+#include <iostream>
+#include <string>
+#include <memory>
+
+namespace bpp {
+
+/**
+ * @brief This iterator calls an external program on each block.
+ */
+class SystemCallMafIterator:
+  public AbstractFilterMafIterator
+{
+  private:
+    std::auto_ptr<OAlignment> alnWriter_;
+    std::string inputFile_;
+    std::auto_ptr<IAlignment> alnReader_;
+    std::string outputFile_;
+    std::string call_;
+
+  public:
+    SystemCallMafIterator(
+        MafIterator* iterator,
+        OAlignment* alnWriter,
+        const std::string& inputFile,
+        IAlignment* alnReader,
+        const std::string& outputFile,
+        const std::string& callCmd) :
+      AbstractFilterMafIterator(iterator),
+          alnWriter_(alnWriter),
+          inputFile_(inputFile),
+          alnReader_(alnReader),
+          outputFile_(outputFile),
+          call_(callCmd)
+    {}
+
+  private:
+    SystemCallMafIterator(const SystemCallMafIterator& iterator) :
+      AbstractFilterMafIterator(0),
+      alnWriter_(0),
+      inputFile_(iterator.inputFile_),
+      alnReader_(0),
+      outputFile_(iterator.outputFile_),
+      call_(iterator.call_)
+    {}
+    
+    SystemCallMafIterator& operator=(const SystemCallMafIterator& iterator)
+    {
+      inputFile_ = iterator.inputFile_;
+      outputFile_ = iterator.outputFile_;
+      call_ = iterator.call_;
+      return *this;
+    }
+
+
+  public:
+    MafBlock* analyseCurrentBlock_() throw (Exception);
+
+};
+
+} // namespace bpp.
+
+#endif //_SYSTEMCALLMAFITERATOR_H_
+
