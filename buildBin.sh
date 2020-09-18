40be72d2968c91757a298ddda2f40837d77cac8b
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2020-09-18 15:29:50 +0200

    Updated static linkage options.

--- buildBin.sh
+++ buildBin.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 arch=`uname -m`
-version=1.3.1beta-1
+version=1.3.1-2
+
+rm CMakeCache.txt 
+ccmake -D CMAKE_INSTALL_PREFIX=$HOME/.local -DBUILD_STATIC=YES ..
+
+make 
 
 strip MafFilter/maffilter
 tar cvzf maffilter-${arch}-bin-static-${version}.tar.gz MafFilter/maffilter
